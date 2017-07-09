47ea1869f5bc87dedb97f8f6638b881ed334bdac
Trevor Davis <tzendou@gmail.com>
2017-07-08 18:55:23 -0700

    added project structure and entities

---
+++ hotel/src/main/java/com/trevor/hotel/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.trevor.hotel.repository;
+
+/**
+ * Created by tdavis on 7/8/17.
+ */
+public class RoomRepository {
+}
---
+++ hotel/src/main/java/com/trevor/hotel/service/ReservationService.java
@@ -0,0 +1,7 @@
+package com.trevor.hotel.service;
+
+/**
+ * Created by tdavis on 7/8/17.
+ */
+public class ReservationService {
+}
